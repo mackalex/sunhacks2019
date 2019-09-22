899585ed0fe0301ad9437e7c455951a1bd51b9b9
Jake Farabee <jfarabee@asu.edu>
2019-09-21 17:47:18 -0700

    implemented map activity

--- app/src/main/java/com/team7/sunhacks2019/MainActivity.kt
+++ app/src/main/java/com/team7/sunhacks2019/MainActivity.kt
@@ -1,12 +1,40 @@
 package com.team7.sunhacks2019
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.esri.arcgisruntime.mapping.ArcGISMap
+import com.esri.arcgisruntime.mapping.Basemap
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // create an ArcGISMap with the BasemapType topographic
+        val map = ArcGISMap(Basemap.Type.TOPOGRAPHIC,
+                34.056295,
+                -117.195800,
+                16)
+
+        // set the map to be displayed in the layout's MapView
+        mapView.map = map
+    }
+
+    override fun onPause() {
+        //override pause behavior to include pausing mapView
+        super.onPause()
+        mapView.pause()
+    }
+    override fun onResume() {
+        //override resume behavior to include resuming mapView
+        super.onResume()
+        mapView.resume()
+    }
+    override fun onDestroy() {
+        //override destroy behavior to include destroying mapView
+        super.onDestroy()
+        mapView.dispose()
     }
 }
